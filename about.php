ac89bade12b81b67223eb8cad01c7099f9814887
susangrattan <grattanwebdev@gmail.com>
2016-03-26 14:49:04 -0600

    I hate IE..

--- about.php
+++ about.php
@@ -4,10 +4,10 @@ include 'includes/navbar.php';
 ?>
 <h1>The Man Behind the Mask</h1>
   <div class="row">
-    <div class="columns medium-6 align-self-middle">
+    <div class="columns small-12 medium-6 align-self-middle">
       <img src="http://i81.photobucket.com/albums/j234/robertg_g/chat/goingdown.jpg" alt="picture of me coming up from a dive" />
     </div>
-    <div class="columns medium-6 align-self-middle">
+    <div class="columns small-12 medium-6 align-self-middle">
       <p class="text-justify">
         My hometown is Port Huron, Michigan.  Home of the most dangerous navigatable waterway in the United States.  While diving, 1,200 foot freighters go right over top of you.  The current fast and unpredictable.  This is where Lake Huron empties into the St. Clair River.  Sarnia, Canada is on the other side of the bridges.  It is impossible to swim against the current here and you have to be super careful it doesn't rip your mask off if you look behind you.
       </p>
